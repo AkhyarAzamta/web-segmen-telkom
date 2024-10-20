cea81b54afb4d211e552b1275393c6e99a45930d
akhyarazamta <akhyar.azamta@gmail.com>
2024-10-20 21:45:50 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+let map;
+let routingControl;
+let routes = window.data; // Ambil data dari data.js
+let selectedRoute = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Inisialisasi peta
+  map = L.map('map').setView([-6.914744, 107.609810], 10);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  // Tambahkan opsi rute ke dropdown
+  const routeSelector = document.getElementById('routeSelector');
+  routes.forEach(route => {
+    const option = document.createElement('option');
+    option.value = route.name;
+    option.textContent = route.name;
+    routeSelector.appendChild(option);
+  });
+
+  // Event listener untuk pemilihan rute
+  routeSelector.addEventListener('change', (e) => {
+    const routeName = e.target.value;
+    selectedRoute = routes.find(route => route.name === routeName);
+
+    if (selectedRoute) {
+      updateMapWithRoute(selectedRoute);
+    }
+  });
+
+  // Event listener untuk menambah waypoint
+  document.getElementById('addWaypointButton').addEventListener('click', () => {
+    if (!selectedRoute) {
+      alert('Pilih rute terlebih dahulu');
+      return;
+    }
+
+    const latLngInput = prompt("Masukkan Latitude dan Longitude dipisahkan dengan koma(,)");
+    const description = prompt("Masukkan Deskripsi untuk lokasi baru:");
+
+    const [lat, lng] = latLngInput.split(',').map(coord => parseFloat(coord.trim()));
+
+    if (!isNaN(lat) && !isNaN(lng) && description) {
+      const newWaypoint = { lat, lng, description };
+      selectedRoute.waypoints.push(newWaypoint);
+      updateMapWithRoute(selectedRoute);
+    } else {
+      alert("Input tidak valid. Pastikan untuk memasukkan koordinat dalam format yang benar dan deskripsi.");
+    }
+  });
+});
+
+function updateMapWithRoute(route) {
+  if (routingControl) {
+    map.removeControl(routingControl);
+  }
+
+  routingControl = L.Routing.control({
+    waypoints: [
+      L.latLng(route.org_latLng.lat, route.org_latLng.lng),
+      ...route.waypoints.map(wp => L.latLng(wp.lat, wp.lng)),
+      L.latLng(route.dst_latLng.lat, route.dst_latLng.lng)
+    ],
+    routeWhileDragging: true,
+    show: false
+  }).addTo(map);
+
+  // Tambah marker untuk start, end, dan waypoint
+  L.marker([route.org_latLng.lat, route.org_latLng.lng]).addTo(map).bindPopup(route.org_latLng.org_site);
+  L.marker([route.dst_latLng.lat, route.dst_latLng.lng]).addTo(map).bindPopup(route.dst_latLng.dst_site);
+
+  route.waypoints.forEach(({ lat, lng, description }) => {
+    L.marker([lat, lng]).addTo(map).bindPopup(`Description: ${description}`);
+  });
+}
---
+++ data.js
@@ -0,0 +1,107 @@
+window.data = [
+  {
+    name: "WJO_SEGMENT_061",
+    org_latLng: { lat: -6.656108, lng: 106.847114, org_site: "BGRCAW01" },
+    dst_latLng: { lat: -6.777657, lng: 106.787172, dst_site: "SKBRYS01" },
+    waypoints: [
+      { lat: -6.777657, lng: 106.787172, description: "Waypoint 1" },
+    ]
+  },
+  {
+    name: "WJO_SEGMENT_043",
+    org_latLng: { lat: -6.870357, lng: 107.097689, org_site: "CAJRYS01" },
+    dst_latLng: { lat: -6.844556, lng: 107.120513, dst_site: "CARCLK01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_053",
+    org_latLng: { lat: -6.825041, lng: 107.127247, org_site: "CAJKAB01" },
+    dst_latLng: { lat: -6.800502, lng: 107.178094, dst_site: "CARKRT01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_054", // baru sampe sini koordinat nya
+    org_latLng: { lat: -6.844556, lng: 107.120513, org_site: "CARCLK01" },
+    dst_latLng: { lat:-6.917419, lng: 106.972024, dst_site: "CAJRCB01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_063", // ini dan kebawah belum di input koordinat nya
+    org_latLng: { lat: -6.870357, lng: 107.097689, org_site: "CAJRYS01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKRSKR01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_087",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "CARKRT01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "CAJRMS01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_001",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRYC03" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW03" },
+    waypoints: []
+  },  
+  {
+    name: "WJO_SEGMENT_002",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRYS01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW03" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_050",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRBS01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKRPLB01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_051",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBBHY01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRYC02" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_052",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBKPJ01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBBHY01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_081",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLW03" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRBS01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_082",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRBS01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBBHY01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_083",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBBRS01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRRK01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_023",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBNLG01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRYC03" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_055",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBAAY01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBNLG01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_056",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLW02" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBAAY01" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_057",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSYK01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW02" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_058",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLB01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW02" },
+    waypoints: []
+  },  {
+    name: "WJO_SEGMENT_060",
+    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRRK01" },
+    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBAAY01" },
+    waypoints: []
+  },
+];
