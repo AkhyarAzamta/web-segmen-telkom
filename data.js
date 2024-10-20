4ccf3d7269383c556c46c9aa00ed7c95557e233c
akhyarazamta <akhyar.azamta@gmail.com>
2024-10-21 00:18:32 +0700

    ok

--- data.js
+++ data.js
@@ -25,83 +25,83 @@ window.data = [
   },  {
     name: "WJO_SEGMENT_063", // ini dan kebawah belum di input koordinat nya
     org_latLng: { lat: -6.870357, lng: 107.097689, org_site: "CAJRYS01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKRSKR01" },
+    dst_latLng: { lat: -6.917419, lng: 106.972024, dst_site: "SKRSKR01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_087",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "CARKRT01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "CAJRMS01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "CARKRT01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "CAJRMS01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_001",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRYC03" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW03" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBRYC03" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBSLW03" },
     waypoints: []
   },  
   {
     name: "WJO_SEGMENT_002",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRYS01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW03" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBRYS01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBSLW03" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_050",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRBS01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKRPLB01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBRBS01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKRPLB01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_051",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBBHY01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRYC02" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBBHY01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBRYC02" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_052",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBKPJ01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBBHY01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBKPJ01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBBHY01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_081",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLW03" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRBS01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBSLW03" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBRBS01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_082",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRBS01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBBHY01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBRBS01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBBHY01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_083",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBBRS01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRRK01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBBRS01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBRRK01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_023",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBNLG01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBRYC03" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBNLG01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBRYC03" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_055",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBAAY01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBNLG01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBAAY01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBNLG01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_056",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLW02" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBAAY01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBSLW02" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBAAY01" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_057",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSYK01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW02" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBSYK01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBSLW02" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_058",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBSLB01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBSLW02" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBSLB01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBSLW02" },
     waypoints: []
   },  {
     name: "WJO_SEGMENT_060",
-    org_latLng: { lat: -6.901744, lng: 107.600810, org_site: "SKBRRK01" },
-    dst_latLng: { lat: -6.920464, lng: 107.629123, dst_site: "SKBAAY01" },
+    org_latLng: { lat: 0, lng: 0, org_site: "SKBRRK01" },
+    dst_latLng: { lat: 0, lng: 0, dst_site: "SKBAAY01" },
     waypoints: []
   },
 ];
